a3665a2f4acee41e2e97e09c03cc851392848abd
sravan277 <sravan27745@gmail.com>
2025-11-19 12:02:00 +0530

    removed get from server

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require("express");
 const app = express();
 
 
-app.get("/", (req, res)=>{
-    res.status(200).json({"Message":"Hello World"});
-})
+
 
 app.listen(3000, (req, res)=>{
     console.log("http://localhost:3000");
